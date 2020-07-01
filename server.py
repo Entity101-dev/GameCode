43d4ecb2c9bd1be81e42fde4d02003e44a72d92b
Entity101-dev <66175273+Entity101-dev@users.noreply.github.com>
2020-07-01 16:16:57 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -2,10 +2,9 @@ import socket
 import argparse
 import threading 
 
-parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
-parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = socket.gethostname())
-parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 9999)
-args = parser.parse_args()
+correct = 0
+
+
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try: 
@@ -13,27 +12,27 @@ try:
 	serv.listen(5)
 except Exception as e:
 	raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
+print("server start")
 
-def on_new_client(client, connection):
-    ip = connection[0]
-    port = connection[1]
-    print(f"THe new connection was made from IP: {ip}, and port: {port}!")
-    conn, addr = serv.accept()
-    while True:
-        data = conn.recv(4096)
-        if not data: break
-        data2 = data.decode('utf-8')
-        if data2 == "156":
-            conn.send(bytes("Correct", "utf-8"))
-        else:
-            conn.send(bytes("Wrong", "utf-8"))
-    conn.close()
-    print('client disconnected')
+
+def on_new_client(client, ip):
+	while True:
+		data = client.recv(4096)
+		if not data: break
+		print(data)
+		if data == bytes("153", "utf-8"):
+			print("wich is correct")
+			client.send(bytes("Correct", "utf-8"))
+			break
+		else:
+			client.send(bytes("Wrong", "utf-8"))
+	client.close()
+	print('client disconnected')
 
 while True:
 	try: 
 		client, ip = serv.accept()
-		threading._start_new_thread(on_new_client,(client, ip))
+		on_new_client(client, ip)
 		print("server started")
 	except KeyboardInterrupt:
 		print(f"Gracefully shutting down the server!")
@@ -41,4 +40,4 @@ while True:
 		print(f"Well I did not anticipate this: {e}")
 		break
 
-sck.close()
+client.close()
